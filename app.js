3c8ffbb1a3d20e0cb17b3e42dffb9cbb78226216
fadhi690mk <88191826+fadhi690mk@users.noreply.github.com>
2023-06-17 18:26:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const dbUrl = process.env.DB_URL;
 const REACT_APP_URL = process.env.REACT_APP_URL;
 
 app.use(cors({
-  origin: REACT_APP_URL,
+  origin: 'https://relaxed-taffy-3d19bd.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: 'Content-Type',
   credentials: true 
